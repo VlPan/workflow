5e296da7af42f660e14735ad5564d38470de6f01
VlPan <vladimirpant.bel@gmail.com>
2017-09-20 08:26:42 +0300

    +CSS and images handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-const sum = require('./sum');
+import sum from './sum';
+import './image_viewer'; // no export of code - just run!
 
 console.log(sum(5,7));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 let config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: "build/"
     },
     module:{ // Preprocessing steps!
         rules: [
             {
                 use: 'babel-loader',
                 test: /\.js$/, // only js files
+            },
+            {
+                //use: ['style-loader','css-loader'], // order is important here!! from right to left
+                loader: ExtractTextPlugin.extract ({
+                    loader: 'css-loader'
+                }),
+                test: /\.css$/
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {limit: 40000}
+                    },
+                    'image-webpack-loader',
+                    //'file-loader'             //ORDER FROM BOTTOM TO TOP
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css') // New big file with style name
+    ]
 }
 
 module.exports = config;
